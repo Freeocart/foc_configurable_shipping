a0d9cecee6c53ef6374db3e38057267e781d8721
Sergey Tominoff <ikenfin@gmail.com>
2021-04-13 16:32:23 +0400

    :wrench: fix wrong language_id value

--- frontend/src/components/Output.js
+++ frontend/src/components/Output.js
@@ -1,13 +1,19 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { ConfigContext } from './ConfigProvider'
+import React, { useContext, useState, useEffect } from "react";
+import { ConfigContext } from "./ConfigProvider";
 
-export default function Output ({ outputName }) {
-  const { state } = useContext(ConfigContext)
-  const [ output, setOutput ] = useState(state)
+export default function Output({ outputName }) {
+  const { state } = useContext(ConfigContext);
+  const [output, setOutput] = useState(state);
 
   useEffect(() => {
-    setOutput(JSON.stringify(state))
-  }, [ state ])
+    setOutput(JSON.stringify(state));
+  }, [state]);
 
-  return <textarea className="Rules__output" name={ outputName } value={ output }></textarea>
+  return (
+    <textarea
+      className="Rules__output"
+      name={outputName}
+      value={output}
+    ></textarea>
+  );
 }
